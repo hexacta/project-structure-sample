7e1dc273ea4e8f9840e48d6eabef3cfd93d72e3c
pomber <pombopombopombo@gmail.com>
2017-03-14 18:31:54 -0300

    Refactor api folder

--- api/src/server.js
+++ api/src/server.js
@@ -2,21 +2,24 @@ import http from "http";
 import express from "express";
 
 const PORT = 8080;
+const DELAY = 2500;
 
-const reverse = s => s.split("").reverse().join("");
-
-async function start() {
-  const app = express();
-  app.server = http.createServer(app);
-
-	app.get("/api/reverse/:something", (req, res) => {
-    const something = req.params.something;
-		const reversed = reverse(something);
-    res.json(reversed);
+const reverse = s => {
+  const out = s.split("").reverse().join("");
+  return new Promise(resolve => {
+    setTimeout(() => resolve(out), DELAY);
   });
+};
 
-  app.server.listen(PORT);
-  console.log(`API started on port ${app.server.address().port}`);
-}
+const app = express();
+app.server = http.createServer(app);
 
-start();
+app.get("/api/reverse/:something", async (req, res) => {
+  const something = req.params.something;
+  console.log(`Reversing "${something}"`);
+  const reversed = await reverse(something);
+  res.json(reversed);
+});
+
+app.server.listen(PORT);
+console.log(`API started on port ${app.server.address().port}`);
